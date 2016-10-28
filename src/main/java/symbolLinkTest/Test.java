1f955fc5b55c656a7a91277a8806f3c1729542c1
tonychen007 <1@1.com>
2016-10-28 18:49:47 +0800

    add symbolic link test

---
+++ src/main/java/symbolLinkTest/Test.java
@@ -0,0 +1,23 @@
+package symbolLinkTest;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+
+@SuppressWarnings("all")
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		Path link = FileSystems.getDefault().getPath("C:/symbol.txt");
+		Path target = FileSystems.getDefault().getPath("Z:/1.txt");
+		if(!Files.isSymbolicLink(link)) {
+			Path sl = Files.createSymbolicLink(link,target);		}
+		
+		// To check if the path is a symbolicLink, LinkOption.NOFOLLOW_LINKS should be passed
+		BasicFileAttributes attr = Files.readAttributes(link, BasicFileAttributes.class,LinkOption.NOFOLLOW_LINKS);
+		boolean ret = attr.isSymbolicLink();
+	}
+}
