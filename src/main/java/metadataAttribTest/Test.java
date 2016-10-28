f52ceea683868b9ea9fe87bcba25d63cd7332d4f
tonychen007 <1@1.com>
2016-10-28 10:14:25 +0800

    convert to maven project

--- src/main/java/metadataAttribTest/Test.java
+++ src/main/java/metadataAttribTest/Test.java
@@ -1,4 +1,4 @@
-package MetadataAttribTest;
+package metadataAttribTest;
 
 import java.io.IOException;
 import java.nio.file.FileStore;
@@ -50,5 +50,6 @@ public class Test {
 		
 		AclFileAttributeView acl = Files.getFileAttributeView(path, AclFileAttributeView.class);
 		List<AclEntry> aclList = acl.getAcl();
+			
 	}
 }
