82b3f2d51c1f6f551231e06a6564f7d57547470b
tonychen007 <1@1.com>
2016-11-19 15:45:44 +0800

    add async socket completion handler

--- src/main/java/asyncSocket/ServerCompletionHandler.java
+++ src/main/java/asyncSocket/ServerCompletionHandler.java
@@ -4,30 +4,41 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.StandardSocketOptions;
 import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ServerCompletionHandler {
 
 	public static void main(String[] args) {
-		try (AsynchronousServerSocketChannel asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open()) {
+		AsynchronousChannelGroup threadGroup = null;
+		ExecutorService executorService = Executors.newCachedThreadPool(Executors.defaultThreadFactory());
+		try {
+			threadGroup = AsynchronousChannelGroup.withCachedThreadPool(executorService, 5);
+		} catch (IOException e1) {
+
+		}
+
+		try (AsynchronousServerSocketChannel asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open(threadGroup)) {
 			if (asynchronousServerSocketChannel.isOpen()) {
 				asynchronousServerSocketChannel.setOption(StandardSocketOptions.SO_RCVBUF, 4 * 1024);
 				asynchronousServerSocketChannel.setOption(StandardSocketOptions.SO_REUSEADDR, true);
 				asynchronousServerSocketChannel.bind(new InetSocketAddress("0.0.0.0", 10086));
 				System.out.println("Waiting for connections ...");
 				asynchronousServerSocketChannel.accept(null, new CompletionHandler<AsynchronousSocketChannel, Void>() {
-					
+
 					final ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-					
+
 					@Override
 					public void completed(AsynchronousSocketChannel result, Void attachment) {
 						asynchronousServerSocketChannel.accept(null, this);
 						try {
 							System.out.println("Incoming connection from: " + result.getRemoteAddress());
-							
+
 							while (result.read(buffer).get() != -1) {
 								buffer.flip();
 								result.write(buffer).get();
@@ -40,7 +51,7 @@ public class ServerCompletionHandler {
 						} catch (IOException | InterruptedException | ExecutionException ex) {
 							System.err.println(ex);
 						} finally {
-							try {								
+							try {
 								result.close();
 							} catch (IOException e) {
 								System.err.println(e);
